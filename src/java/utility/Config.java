448af9baae005ee61657324df3bf620c2e369479
david <david@Davide>
2024-05-10 17:26:26 +0200

    cambio dati password db mysql

--- src/java/utility/Config.java
+++ src/java/utility/Config.java
@@ -2,12 +2,12 @@ package utility;
 
 public class Config {
     
-    public static String url="http://localhost:8084/DiMauroRating";    
-    //public static String url="https://app.infomediatek.it/DiMauroRating";    
+    //public static String url="http://localhost:8084/DiMauroRating";    
+    public static String url="https://app.infomediatek.it/DiMauroRating";    
     
     public static String db_url = "jdbc:mysql://195.231.15.77:3306/dimauro_rating";
     public static String db_login="infomedia_new";           
-    public static String db_password="Rob.Dav.1986";           
+    public static String db_password="Comisetti.2024!";           
     
    /*public static String mittente_email="info@dimauroog.it";
     public static String mittente_password="54LKJ7j3df53fRCFa";
